cb4cfecb55d93fe80108c471888c19e78e116cff
IrasNout <invisiballos@gmail.com>
2026-05-12 08:23:29 +0500

    m_36_Task_10 fixed

--- m_36_Task_10.1.js
+++ m_36_Task_10.1.js
@@ -1,4 +1,4 @@
 const num_input = +prompt('Введите число');
 alert(`Введённое число - ${num_input}
-    Квадрат числа - ${num_input ** 2}`)
+    Квадрат числа - ${num_input ** 2}`
     Куб числа - ${num_input ** 3}`);
